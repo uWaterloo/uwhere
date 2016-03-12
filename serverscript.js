e2e7e8ce18e38516e4e0a3986563625e70b2e006
abusefreewill <kealansisler@gmail.com>
2016-03-12 15:04:52 -0500

    Updated user branch from Portal workspace (server script)

---
+++ serverscript.js
@@ -0,0 +1,9 @@
+function getLocations() {
+    var options = 'SELECT * FROM ' + args.Get('value');
+    var queryResult = db.Execute(options);
+    var rows = JSON.parse(queryResult);
+    if (rows.length > 0 && typeof rows[0].Error != 'undefined') {
+        return '{"status":"noTable"};
+    }
+    return queryResult;
+}
